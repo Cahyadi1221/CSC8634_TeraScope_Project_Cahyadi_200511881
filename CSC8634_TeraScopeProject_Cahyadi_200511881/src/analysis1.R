81ac9d08f2cf9360e80b8664da5934c3ec6a22e5
cahyadi0511881 <c.cahyadi2@newcastle.ac.uk>
2021-01-21 20:21:22 +0000

    Subset the data back to each event name and extract the correlation matrix from each data subset

--- CSC8634_TeraScopeProject_Cahyadi_200511881/src/analysis1.R
+++ CSC8634_TeraScopeProject_Cahyadi_200511881/src/analysis1.R
@@ -19,3 +19,42 @@ applicationCheckpointDuplicates
 
 graphicalSummaries1 = pairs(masterData[,c(6,9:12)])
 cache('graphicalSummaries1')
+
+# Numerical Summaries of the horizontal data
+cor(masterData[,c(6,9:12,15:19,22:26,29:33,36:43)])
+
+# Graphical summaries on the vertical data
+pairs(verticalData_Raw[,c(7,10:12)])
+
+# Get the correlation matrix from the numerical columns
+cor(verticalData_Raw[,c(7,10:12)])
+
+# Subsetting the data via event Name and check the Correlation Matrix for each
+
+# Total Render
+totalRenderData = subset(verticalData_Raw, eventName == "TotalRender")
+cor(totalRenderData[,c(7,10:13)])
+
+# Saving Config
+savingConfigData = subset(verticalData_Raw, eventName == "Saving Config")
+cor(savingConfigData[,c(7,10:13)])
+# The correlation between the duration and the Power Draw is really low, quite
+# possibly caused by the interpolation made to record the GPU condition of this
+# particular event name
+
+# Render 
+renderData = subset(verticalData_Raw, eventName == "Render")
+cor(renderData[,c(7,10:13)])
+
+
+# Tiling
+tilingData  = subset(verticalData_Raw, eventName == "Tiling")
+cor(tilingData[,c(7,10:13)])
+
+# Uploading
+uploadingData = subset(verticalData_Raw, eventName == "Uploading")
+cor(uploadingData[,c(7,10:13)])
+
+
+
+
