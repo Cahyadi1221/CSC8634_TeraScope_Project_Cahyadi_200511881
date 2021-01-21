6b65c879c213118ea1e6cff242c1bbe51b7ea75e
cahyadi0511881 <c.cahyadi2@newcastle.ac.uk>
2021-01-21 01:30:50 +0000

    Use the pairs function to check for any trend

--- CSC8634_TeraScopeProject_Cahyadi_200511881/src/analysis1.R
+++ CSC8634_TeraScopeProject_Cahyadi_200511881/src/analysis1.R
@@ -2,5 +2,19 @@
 library(dplyr)
 library(ProjectTemplate)
 library(lubridate)
+library(ggplot2)
 load.project()
 
+# Check the duplicates on the original data
+
+applicationCheckpointDuplicates = anyDuplicated(Copy.of.application.checkpoints)
+# Run the variable to check the number of duplicates in the original data
+applicationCheckpointDuplicates
+# There are 130821 duplicates within this data set
+
+# Therefore in the wrangling process, the usage of unique(data) to avoid processing 
+# duplicates is validated. 
+
+# Check for the total Render data, the correlation using pairs function
+
+graphicalSumamries1 = pairs(masterData[,c(6,9:12)])
