fea8ca20f9f5fac5504ce3677bb8c6379c3fb85a
cahyadi0511881 <c.cahyadi2@newcastle.ac.uk>
2021-01-22 18:46:47 +0000

    Delete Unneccesarry cache

--- CSC8634_TeraScopeProject_Cahyadi_200511881/src/analysis1.R
+++ CSC8634_TeraScopeProject_Cahyadi_200511881/src/analysis1.R
@@ -17,8 +17,7 @@ applicationCheckpointDuplicates
 
 # Check for the total Render data, the correlation using pairs function
 
-graphicalSummaries1 = pairs(masterData[,c(6,9:12)], main = "Total Render Plots")
-cache('graphicalSummaries1')
+pairs(masterData[,c(6,9:12)], main = "Total Render Plots")
 
 # Numerical Summaries of the horizontal data
 cor(masterData[,c(6,9:12,15:19,22:26,29:33,36:43)])
@@ -32,14 +31,14 @@ cor(verticalData_Raw[,c(7,10:12)])
 # Subsetting the data via event Name and check the Correlation Matrix for each
 
 # Total Render
-totalRenderData = subset(verticalData_Raw, eventName == "TotalRender")
+totalRenderData = subset(verticalData_merged_Raw, eventName == "TotalRender")
 cor(totalRenderData[,c(7,10:13)])
 # Extracting the ColMeans for the data set
 numericalAvgTotalRender = colMeans(totalRenderData[,c(7,10:13)])
 numericalAvgTotalRender
 
 # Saving Config
-savingConfigData = subset(verticalData_Raw, eventName == "Saving Config")
+savingConfigData = subset(verticalData_merged_Raw, eventName == "Saving Config")
 cor(savingConfigData[,c(7,10:13)])
 # The correlation between the duration and the Power Draw is really low, quite
 # possibly caused by the interpolation made to record the GPU condition of this
@@ -56,7 +55,7 @@ numericalAvgSavingConfig
 
 
 # Render 
-renderData = subset(verticalData_Raw, eventName == "Render")
+renderData = subset(verticalData_merged_Raw, eventName == "Render")
 cor(renderData[,c(7,10:13)])
 # The correlation matrix here provide a similar result to that of the total Render
 # event name. This is probably due to the characteristic of the render event name
@@ -73,7 +72,7 @@ numericalAvgRender
 # GPU Mem Util Perc = 37.43622 %
 
 # Tiling
-tilingData  = subset(verticalData_Raw, eventName == "Tiling")
+tilingData  = subset(verticalData_merged_Raw, eventName == "Tiling")
 cor(tilingData[,c(7,10:13)])
 # Again, having the similar correlation matrix result with the Saving Config 
 # event name, quite possibly because we are using the interpolation of gpu condition
@@ -91,7 +90,7 @@ numericalAvgTiling
 
 
 # Uploading
-uploadingData = subset(verticalData_Raw, eventName == "Uploading")
+uploadingData = subset(verticalData_merged_Raw, eventName == "Uploading")
 cor(uploadingData[,c(7,10:13)])
 
 # Extracting the colMeans for the data set
@@ -104,6 +103,8 @@ numericalAvgUploading
 # GPU Temp          = 39.533123 Celcius
 # GPU Util Perc     = 12.323613 %
 # GPU Mem Util Perc = 5.750262 %
+
+
 
 
 
